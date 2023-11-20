4b6808fc7d6a854a3b9f7a5316a066468b34f677
Srdan <118986032+sardinka88@users.noreply.github.com>
2023-11-20 16:59:03 +0100

    added solution tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, numTimes) {
+    let string = "";
+    if (numTimes < 0) return "ERROR";
+    for (i = 0; i < numTimes; i++) {
+        string += word;
+    }
+    return string;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
