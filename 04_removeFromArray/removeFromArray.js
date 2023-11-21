d80270c2a8b8d21264aa85d41ae3f60620e44344
Srdan <118986032+sardinka88@users.noreply.github.com>
2023-11-21 14:56:31 +0100

    changed variable names to match in both solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function (array, ...numbers) {
-  const filtered = array.filter(element=> !numbers.includes(element))  
+const removeFromArray = function (array, ...args) {
+  const filtered = array.filter(item => !args.includes(item))  
   return filtered;
 }
 
