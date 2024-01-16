61597b3e04f5a07104323c03df8af852bfbb0b07
Srdan <118986032+sardinka88@users.noreply.github.com>
2024-01-16 17:05:13 +0100

    Add solution and run tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS';
+    let result = [0, 1];
+    for (let i=2; i <= num; i++) {
+        result[i] = result[i-1] + result[i-2];
+    }
+    return result[num];
 };
 
 // Do not edit below this line
