14bfa3876e5e142db361044615b6fa14986f4d07
Srdan <118986032+sardinka88@users.noreply.github.com>
2024-01-16 14:47:49 +0100

    Add solution and run tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let processedString = inputString.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
+    return processedString === processedString.split('').reverse().join("");
 };
 
 // Do not edit below this line
