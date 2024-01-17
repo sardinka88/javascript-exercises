f08355a4dc53fd923c06e4e636aca06d4abd0c2b
Srdan <118986032+sardinka88@users.noreply.github.com>
2024-01-17 09:58:57 +0100

    Add solution using forEach

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = [];
+    array.forEach(obj => {
+        titles.push(obj.title);
+    })
+    return titles;
 };
 
 // Do not edit below this line
